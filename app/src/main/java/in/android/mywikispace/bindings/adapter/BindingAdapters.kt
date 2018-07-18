18d986d1acda30702b1ae9b27ffd3f5aa31f119d
Kumar Gaurav <kumar.gaurav4@harman.com>
2018-07-18 15:47:23 +0530

    a method for binding an adapter to RecyclerView

--- app/src/main/java/in/android/mywikispace/bindings/adapter/BindingAdapters.kt
+++ app/src/main/java/in/android/mywikispace/bindings/adapter/BindingAdapters.kt
@@ -1,5 +1,6 @@
 package `in`.android.mywikispace.bindings.adapter
 
+import `in`.android.mywikispace.ui.adapter.HomeAdapter
 import android.databinding.BindingAdapter
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
@@ -7,14 +8,23 @@ import android.support.v7.widget.RecyclerView
 
 class BindingAdapters {
 
-    @BindingAdapter("toggleLayoutManager")
-    fun toggleLayoutManager(recyclerView: RecyclerView, isGrid: Boolean){
-        val lM: RecyclerView.LayoutManager
-        if(isGrid != null && isGrid){
-            lM = GridLayoutManager(recyclerView.context, 2)
-        }else{
-            lM = LinearLayoutManager(recyclerView.context)
+    companion object {
+
+        @JvmStatic @BindingAdapter("toggleLayoutManager")
+        fun toggleLayoutManager(recyclerView: RecyclerView, isGrid: Boolean){
+            val lM: RecyclerView.LayoutManager
+            if(isGrid != null && isGrid){
+                lM = GridLayoutManager(recyclerView.context, 2)
+            }else{
+                lM = LinearLayoutManager(recyclerView.context)
+            }
+            recyclerView.layoutManager = lM
         }
-        recyclerView.layoutManager = lM
+
+        @BindingAdapter("adapter")
+        fun bindAdapter(rv: RecyclerView, adapter: RecyclerView.Adapter<out RecyclerView.ViewHolder>) {
+            rv.adapter = adapter
+        }
     }
+
 }
